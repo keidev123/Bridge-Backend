53e172d90cb76e74f971a300fe185f5e1734fbb6
keidev123 <keidev60@gmail.com>
2025-06-17 22:15:27 +0300

    Initial commit

---
+++ index.ts
@@ -0,0 +1,21 @@
+import cors from "cors";
+import dotenv from "dotenv";
+import express from "express";
+import http from "http";
+import { PORT } from "./config";
+
+// Load environment variables from .env file
+dotenv.config();
+
+// Create an instance of the Express application
+const app = express();
+
+// Set up Cross-Origin Resource Sharing (CORS) options
+app.use(cors());
+
+const server = http.createServer(app);
+
+// Start the Express server to listen on the specified port
+server.listen(PORT, () => {
+  console.log(`Bridge server is running on port ${PORT}`);
+});
